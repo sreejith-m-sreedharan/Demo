1aff311433b8c4074e47c9c42e90b5d959aa7e5e
Sreejith Sreedaharan <sreejith.m.sreedharan@gmail.com>
2021-05-30 20:31:31 +0530

    custom

---
+++ UI/Custom1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+
+import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+      HttpClientModule,
+      BrowserModule,
+      BrowserAnimationsModule,
+      SharedModule
+      ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
